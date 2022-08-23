3aa2c7510543c7a21c0ce2c50c11f3e00f16322a
u296 <u296b3g@gmail.com>
2022-08-23 23:55:51 +0200

    try to leave when no people are in call

--- src/eventhandler.rs
+++ src/eventhandler.rs
@@ -24,4 +24,72 @@ impl EventHandler for Handler {
         println!("READY");
         println!("git hash: {}", env!("GIT_HASH"));
     }
+
+    #[instrument(skip(ctx))]
+    async fn voice_state_update(&self, ctx: Context, old: Option<serenity::model::voice::VoiceState>, new: serenity::model::voice::VoiceState) {
+
+        match new.guild_id {
+            Some(guild_id) => {
+                let manager = songbird::get(&ctx).await.unwrap();
+
+                match manager.get(guild_id) {
+                    Some(call) => {
+                        info!("guild has handler");
+
+                        let lock = call.lock().await;
+
+                        match lock.current_channel() {
+                            Some(connected_channel_id) => {
+                                info!("handler is connected");
+                                let voice_states = ctx.cache.guild_field(guild_id, |guild| guild.voice_states.clone()).unwrap();
+
+                                for (user_id, voice_state) in voice_states.iter() {
+                                    if voice_state.channel_id.map(Into::into) == Some(connected_channel_id) {
+                                        let user = match user_id.to_user(&ctx).await {
+                                            Ok(u) => u,
+                                            Err(e) => {
+                                                error!("error getting user: {}", e);
+                                                return;
+                                            }
+                                        };
+
+                                        if !user.bot {
+                                            return; // there is a real person in the call, we shouldn't leave
+                                        }
+                                    }
+                                }
+
+                                
+
+                                
+                            }, 
+                            None => {
+                                ()
+                            }
+                        }
+                        
+                        info!("no real people in call, removing handler");
+
+                        
+                        
+
+                        match manager.remove(guild_id).await {
+                            Ok(()) => (),
+                            Err(e) => {
+                                error!("failed to remove handler: {}", e);
+                            }
+                        }
+
+                    },
+                    None => {
+                        info!("guild has no handler");
+                    }
+                }
+                
+            },
+            None => {
+                warn!("voice state update didn't contain guild id!");
+            }
+        }
+    }
 }
